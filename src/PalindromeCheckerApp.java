daaaa9af6adf4284032520a791303a2838f9aa32
sirivennelapaladi3010 <sirivennelapaladi@gmail.com>
2026-02-25 14:33:30 +0530

    Usecase1 is Done

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,7 @@
 public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to palindrome app");
+        System.out.println("version 1.0 system initialised successfully");
+    }
+
 }
