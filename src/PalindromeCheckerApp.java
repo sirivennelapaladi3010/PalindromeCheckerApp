03b7fed723888ad890da60e1428ab1d7ac29e069
sirivennelapaladi3010 <sirivennelapaladi@gmail.com>
2026-02-25 15:50:47 +0530

    Usecase6 is Done

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,18 +1,25 @@
 import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
+
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
 
         String word = "radar";
+
         Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
 
         for (int i = 0; i < word.length(); i++) {
-            stack.push(word.charAt(i));
+            char ch = word.charAt(i);
+            stack.push(ch);
+            queue.add(ch);
         }
 
         boolean isPalindrome = true;
 
         for (int i = 0; i < word.length(); i++) {
-            if (word.charAt(i) != stack.pop()) {
+            if (!stack.pop().equals(queue.remove())) {
                 isPalindrome = false;
                 break;
             }
@@ -24,5 +31,4 @@ public class PalindromeCheckerApp {
             System.out.println(word + " is not a Palindrome.");
         }
     }
-
 }
