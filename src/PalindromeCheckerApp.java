97058bb60c3e2d9be8a21a19a1318fc6f20f35d4
sirivennelapaladi3010 <sirivennelapaladi@gmail.com>
2026-02-25 15:24:30 +0530

    Usecase3 is Done

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,17 +1,17 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
 
-        String word = "madam";
+        String original = "level";
         String reversed = "";
 
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed = reversed + word.charAt(i);
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
         }
 
-        if (word.equals(reversed)) {
-            System.out.println(word + " is a Palindrome.");
+        if (original.equals(reversed)) {
+            System.out.println(original + " is a Palindrome.");
         } else {
-            System.out.println(word + " is not a Palindrome.");
+            System.out.println(original + " is not a Palindrome.");
         }
     }
 
