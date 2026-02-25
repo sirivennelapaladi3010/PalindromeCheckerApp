d37f7fe28527e4d0a1cc953a6f8975baeb826724
sirivennelapaladi3010 <sirivennelapaladi@gmail.com>
2026-02-25 16:32:08 +0530

    Usecase11 modified file

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,20 +1,37 @@
+class PalindromeChecker {
+
+    public boolean checkPalindrome(String word) {
+
+        char[] characters = word.toCharArray();
+        int start = 0;
+        int end = characters.length - 1;
+
+        while (start < end) {
+            if (characters[start] != characters[end]) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
 
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
 
-        String input = "A man a plan a canal Panama";
-        String normalized = input.replaceAll("\\s+", "").toLowerCase();
+        String word = "radar";
 
-        String reversed = "";
+        PalindromeChecker checker = new PalindromeChecker();
+        boolean result = checker.checkPalindrome(word);
 
-        for (int i = normalized.length() - 1; i >= 0; i--) {
-            reversed = reversed + normalized.charAt(i);
-        }
-
-        if (normalized.equals(reversed)) {
-            System.out.println(input + " is a Palindrome (ignoring case and spaces).");
+        if (result) {
+            System.out.println(word + " is a Palindrome.");
         } else {
-            System.out.println(input + " is not a Palindrome (ignoring case and spaces).");
+            System.out.println(word + " is not a Palindrome.");
         }
     }
+
+
 }
