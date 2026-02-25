f4752b14b2a4b634eb899c0fa726efa91e8d4242
sirivennelapaladi3010 <sirivennelapaladi@gmail.com>
2026-02-25 15:45:17 +0530

    Usecase5 is Done

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,20 +1,21 @@
+import java.util.Stack;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
 
         String word = "radar";
-        char[] characters = word.toCharArray();
+        Stack<Character> stack = new Stack<>();
 
-        int start = 0;
-        int end = characters.length - 1;
+        for (int i = 0; i < word.length(); i++) {
+            stack.push(word.charAt(i));
+        }
+
         boolean isPalindrome = true;
 
-        while (start < end) {
-            if (characters[start] != characters[end]) {
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) != stack.pop()) {
                 isPalindrome = false;
                 break;
             }
-            start++;
-            end--;
         }
 
         if (isPalindrome) {
